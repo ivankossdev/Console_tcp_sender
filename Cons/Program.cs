5bd00435d251eae6e6e2ce05ad348cef67d430cb
Ivan <98459118+ivankossdev@users.noreply.github.com>
2022-08-23 23:42:49 +0300

    List IP address

--- Cons/Program.cs
+++ Cons/Program.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Net.Sockets;
 
 namespace Cons
 {
     class Program
     {
-        private const int port = 10500;
-        private const string server = "192.168.0.231";
-
         static void Main(string[] args)
         {
+            
             String message = "";
             Console.WriteLine("Программа печатает входыне аргументы");
+            List<string> ipAddress = new List<string>()
+            {
+                "192.168.0.173",
+                "192.168.0.221",
+                "192.168.0.204"
+            };
+            List<object> threadMessage = new List<object>();
+
             if (args.Length == 0)
             {
                 Console.WriteLine("Нет аргументов");
@@ -28,50 +35,45 @@ namespace Cons
                     message += $"{s} ";
                 }
 
-                //string text = "Р—Р°РєР°Р· Р·РІРѕРЅРєР° С‚РµС…РЅРёС‡РµСЃРєРѕР№ РїРѕРґРґРµСЂР¶РєРё";
-                
-                //Encoding utf8 = Encoding.GetEncoding("Windows-1251");
-                //Encoding win1251 = Encoding.GetEncoding("cp866");
-
-                //byte[] utf8Bytes = win1251.GetBytes(message);
-                //byte[] win1251Bytes = Encoding.Convert(utf8, win1251, utf8Bytes);
-                
-                //Console.WriteLine(win1251.GetString(win1251Bytes));
-                //Console.WriteLine(text);
-
-                //Console.ReadLine();
-                
-                try
+                foreach(string ip in ipAddress)
                 {
-                    TcpClient client = new TcpClient();
-                    client.Connect(server, port);
-
-                    byte[] data = Encoding.UTF8.GetBytes(message);
-                    
-
-                    StringBuilder response = new StringBuilder();
-                    NetworkStream stream = client.GetStream();
-
-                    stream.Write(data, 0, data.Length);
-
-                    Console.WriteLine("Sent: {0}", message);
-
-                    Console.WriteLine(response.ToString());
-
-                    stream.Close();
-                    client.Close();
+                    send(10500, ip, message);
                 }
-                catch (SocketException e)
-                {
-                    Console.WriteLine("SocketException: {0}", e);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Exception: {0}", e.Message);
-                }
-                
-                Console.WriteLine("Запрос завершен...");
             }
+        }
+ 
+        static void send(int port, string server, string message)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(server, port);
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+
+                StringBuilder response = new StringBuilder();
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                Console.WriteLine(response.ToString());
+
+                stream.Close();
+                client.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+
+            Console.WriteLine("Запрос завершен...");
         }
     }
 }
